a963ac7256d47d91854f29d79f4e0d6cf77678d5
Radek Stangel <radek@radekstangel.com>
2016-08-17 14:50:18 +0200

    Create webpack based workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+
 module.exports = {
-  entry: './src/app.jsx',
+  cache: true,
+  entry: {
+    'css/appshell.css': './src/appshell/appshell.scss',
+    'css/content.css': './src/content/content.scss',
+    'js/content.js': './src/content/content.jsx',
+  },
   output: {
-    path: 'js',
-    filename: 'app.js',
+    path: __dirname,
+    filename: '[name]',
   },
   module: {
     loaders: [
       {
         test: /(\.jsx|\.js)$/,
         loader: 'babel-loader',
-        include: __dirname + '/src',
-      }
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
+        include: path.join(__dirname, 'src'),
+      },
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('[name]'),
+    new LiveReloadPlugin()
+  ]
 };
