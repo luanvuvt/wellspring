2885e473f71d1b97aadecbcf1c3b61f4fe845dea
Radek Stangel <radek@radekstangel.com>
2016-08-18 11:57:26 +0200

    Create the main application file with a stylesheet architect

---
+++ src/content/content.jsx
@@ -0,0 +1,19 @@
+/**
+ *   Content :: The Main Application File
+ * =============================================================================
+ */
+
+// import all components
+import Button from '../components/button/button.jsx';
+
+// import all content types
+// import Page from './page/page.jsx';
+// import Posts from './posts/posts.jsx';
+// import Single from './single/single.jsx';
+
+ReactDOM.render(
+  <div>
+    <Button>Click Me Now</Button>
+  </div>,
+  document.getElementById('content')
+);
