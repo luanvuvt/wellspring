14421f0bc7aa4ec4bd0577e138f3462e28724de2
Radek Stangel <radek@radekstangel.com>
2016-08-18 13:22:40 +0200

    Add page content type

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -3,17 +3,12 @@
  * =============================================================================
  */
 
-// import all components
-import Button from '../components/button/button.jsx';
-
 // import all content types
-// import Page from './page/page.jsx';
+import Page from './page/page.jsx';
 // import Posts from './posts/posts.jsx';
 // import Single from './single/single.jsx';
 
 ReactDOM.render(
-  <div>
-    <Button>Click Me Now</Button>
-  </div>,
+  <Page/>,
   document.getElementById('content')
 );
---
+++ src/content/page/page.jsx
@@ -0,0 +1,16 @@
+/**
+ *   Page Content Type
+ * =============================================================================
+ */
+
+// import components
+import Button from '../../components/button/button.jsx';
+
+const Page = () => (
+  <article className="page">
+    <h1>Page Title</h1>
+    <Button>Page Action</Button>
+  </article>
+)
+
+export default Page
