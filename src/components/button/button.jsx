45178fdcb4fb916bfb6894d382f44e2a01528304
Radek Stangel <radek@radekstangel.com>
2016-08-18 11:55:28 +0200

    Create simple button component with styles

---
+++ src/components/button/button.jsx
@@ -0,0 +1,12 @@
+/**
+ *   Button Component
+ * =============================================================================
+ */
+
+const Button = (props) => (
+  <a className="btn" href="#" >
+    {props.children}
+  </a>
+)
+
+export default Button;
