8a04ed8d5b063c5cd321bc39053bc0390282fbda
Radek Stangel <radek@radekstangel.com>
2016-08-18 13:29:24 +0200

    Add single post content type

---
+++ src/content/single/single.jsx
@@ -0,0 +1,16 @@
+/**
+ *   Single Post Content Type
+ * =============================================================================
+ */
+
+// import components
+import Button from '../../components/button/button.jsx';
+
+const Single = () => (
+  <article className="page">
+    <h1>Single Post Title</h1>
+    <Button>Single Post Action</Button>
+  </article>
+)
+
+export default Single
