26dc3a94da6fbac45e136eb9fe7d57103225cfe2
Radek Stangel <radek@radekstangel.com>
2016-08-18 09:09:57 +0200

    Use local React libraries instead of CDN

--- inc/assets.php
+++ inc/assets.php
@@ -8,39 +8,49 @@
  */
 
 /**
- * Remove jQuery from the front-end
+ * Remove jQuery Migrate from the front-end
  *
  * @since 1.0.0
  */
-function wellspring_remove_scripts($scripts) {
+function wellspring_remove_jquery_migrate( &$scripts ) {
     if ( !is_admin() ) {
-        // wp_deregister_script( 'jquery' );
-        // wp_deregister_script( 'wp-embed' );
         $scripts->remove( 'jquery' );
         $scripts->add( 'jquery', false, array( 'jquery-core' ), null, true );
     }
 }
-//add_filter( 'init', 'wellspring_remove_scripts' );
+add_filter( 'wp_default_scripts', 'wellspring_remove_jquery_migrate' );
 
 /**
- * Enqueues fonts, stylesheet and scripts.
+ * App shell assets
+ *
+ * Enqueues all application shell assets
  *
  * @since 1.0.0
  */
-function wellspring_scripts() {
+function wellspring_appshell_assets() {
 	// wellspring appshell styles
-	wp_enqueue_style( 'wellspring-appshell', get_template_directory_uri() . '/dist/appshell.css', array(), THEME_VERSION  );
+	wp_enqueue_style( 'wellspring-appshell', get_template_directory_uri() . '/dist/appshell.css', array(), THEME_VERSION );
+}
+add_action( 'wp_enqueue_scripts', 'wellspring_appshell_assets' );
 
-	// react
-	wp_enqueue_script( 'react', 'https://npmcdn.com/react@15.3.0/dist/react.min.js', array(), null, true );
-
-	// react dom
-	wp_enqueue_script( 'react-dom', 'https://npmcdn.com/react-dom@15.3.0/dist/react-dom.min.js', array(), null, true );
-
+/**
+ * Content assets
+ *
+ * Enqueues all content assets to the footer
+ *
+ * @since 1.0.0
+ */
+function wellspring_content_assets() {
 	// wellspring content styles
 	wp_enqueue_style( 'wellspring-content', get_template_directory_uri() . '/dist/content.css', array(), THEME_VERSION );
+
+	// react
+	wp_enqueue_script( 'react', get_template_directory_uri() . '/dist/vendor/react.min.js', array(), '15.3.0', true );
+
+	// react dom
+	wp_enqueue_script( 'react-dom', get_template_directory_uri() . '/dist/vendor/react-dom.min.js', array(), '15.3.0', true );
 
 	// wellspring content bundle
 	wp_enqueue_script( 'wellspring-content', get_template_directory_uri() . '/dist/content.js', array('react', 'react-dom'), THEME_VERSION, true );
 }
-add_action( 'wp_enqueue_scripts', 'wellspring_scripts' );
+add_action( 'wp_footer', 'wellspring_content_assets' );
