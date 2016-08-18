6f981c47ece3edfd842745646b25ff3e94e7e25b
Radek Stangel <radek@radekstangel.com>
2016-08-18 13:25:59 +0200

    Add posts content type

---
+++ src/content/posts/posts.jsx
@@ -0,0 +1,16 @@
+/**
+ *   Posts Content Type
+ * =============================================================================
+ */
+
+// import components
+import Button from '../../components/button/button.jsx';
+
+const Posts = () => (
+  <article className="posts">
+    <h1>Blog Posts</h1>
+    <Button>Blog Posts Action</Button>
+  </article>
+)
+
+export default Posts
