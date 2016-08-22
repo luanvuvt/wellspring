e62083bb86e0df418f1d9823fb0a5dbcad64ec19
Radek Stangel <radek@radekstangel.com>
2016-08-22 10:20:07 +0200

    Create basic routing

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -3,12 +3,19 @@
  * =============================================================================
  */
 
+import { Router, Route, browserHistory } from 'react-router'
+
 // import all content types
+import Home from './home/home.jsx';
 import Page from './page/page.jsx';
-// import Posts from './posts/posts.jsx';
-// import Single from './single/single.jsx';
+import Posts from './posts/posts.jsx';
+import Single from './single/single.jsx';
 
 ReactDOM.render(
-  <Page/>,
+  <Router history={browserHistory}>
+    <Route path="/" component={Home} />
+    <Route path="blog" component={Posts} />
+    <Route path="blog/:postID" component={Single} />
+  </Router>,
   document.getElementById('content')
 );
