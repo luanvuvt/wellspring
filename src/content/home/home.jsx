98fab46f8c81e0ad3058112a041df18d850feae4
Radek Stangel <radek@radekstangel.com>
2016-08-22 10:05:05 +0200

    Add home page component

---
+++ src/content/home/home.jsx
@@ -0,0 +1,16 @@
+/**
+ *   Home Page Content Type
+ * =============================================================================
+ */
+
+// import components
+import Button from '../../components/button/button.jsx';
+
+const Home = () => (
+  <article className="home">
+    <h1>Home Page Title</h1>
+    <Button>Home Page Action</Button>
+  </article>
+)
+
+export default Home
