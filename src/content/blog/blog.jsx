a7347c28844b040345ad6fee09fe74b0b05c66c2
Radek Stangel <radek@radekstangel.com>
2016-08-23 12:00:36 +0200

    Rename Posts to Blog

---
+++ src/content/blog/blog.jsx
@@ -0,0 +1,16 @@
+/**
+ *   Blog Page Content Type
+ * =============================================================================
+ */
+
+// import components
+import Post from '../post/post.jsx';
+
+const Blog = () => (
+  <div>
+    <Post />
+    <Post />
+  </div>
+)
+
+export default Blog
