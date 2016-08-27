d2148c4528d1de2966ce5a97f34e7d485c03a313
Radek Stangel <radek@radekstangel.com>
2016-08-27 14:43:24 +0200

    Create basic Excerpt component

---
+++ src/components/excerpt/excerpt.jsx
@@ -0,0 +1,13 @@
+/**
+ *   Post Excertpt Component
+ * =============================================================================
+ */
+
+const Excerpt = (props) => (
+  <article className="excerpt">
+    <h1 className="excerpt__title" dangerouslySetInnerHTML={{__html: props.title}} />
+    <section className="excerpt__entry" dangerouslySetInnerHTML={{__html: props.content}} />
+  </article>
+)
+
+export default Excerpt
