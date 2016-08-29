90ee7d8dca3bfb89e70f71c9f4331c51a8802eac
Radek Stangel <radek@radekstangel.com>
2016-08-29 14:03:37 +0200

    Fetch and render data into Page container

---
+++ src/containers/page/page.jsx
@@ -0,0 +1,46 @@
+/**
+ *   Page Container
+ * =============================================================================
+ */
+
+// components
+// import Button from '../../components/button/button.jsx';
+
+class Page extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      page: []
+    };
+  }
+
+  componentDidMount() {
+    // fetch latest posts
+    fetch('//' + window.location.hostname + '/wp-json/wp/v2/pages/?slug=' + this.props.params.slug)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          page: responseData
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Direct: {this.state.page.title}</h2>
+        {this.state.page.map((current) =>
+          <article className="page">
+            <h1 className="page__title" dangerouslySetInnerHTML={{__html: current.title.rendered}} />
+            <section className="page__entry" dangerouslySetInnerHTML={{__html: current.excerpt.rendered}} />
+          </article>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Page
