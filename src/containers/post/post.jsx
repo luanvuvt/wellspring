c923a627a41e182642ac55b9c24172c4cbc66038
Radek Stangel <radek@radekstangel.com>
2016-08-29 14:04:12 +0200

    Fetch and render data into the Post container

--- src/containers/post/post.jsx
+++ src/containers/post/post.jsx
@@ -1,30 +1,49 @@
 /**
- *   Single Post Content Type
+ *   Single Post Container
  * =============================================================================
  */
 
-// import components
+// components
 import Button from '../../components/button/button.jsx';
 
-const Post = () => (
-  <article className="post">
-    <h1 className="post__title">Metamorphosis</h1>
-    <p className="post__intro">
-      One morning, as Gregor Samsa was waking up from anxious dreams,
-      he discovered that in bed he had been changed into a monstrous verminous bug.
-      He lay on his armour-hard back and saw, as he lifted his head up a little,
-      his brown, arched abdomen divided up into rigid bow-like sections.
-    </p>
-    <p>
-      From this height the blanket, just about ready to slide off completely, could hardly stay in place.
-      His numerous legs, pitifully thin in comparison to the rest of his circumference,
-      flickered helplessly before his eyes.
-    </p>
-    <section className="post__nav">
-      <Button>Previous Post</Button>
-      <Button>Next Post</Button>
-    </section>
-  </article>
-)
+class Post extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      post: []
+    };
+  }
+
+  componentDidMount() {
+    // fetch latest posts
+    fetch('//' + window.location.hostname + '/wp-json/wp/v2/posts/?slug=' + this.props.params.slug)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          post: responseData
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.post.map((current) =>
+          <article className="post">
+            <h1 className="post__title" dangerouslySetInnerHTML={{__html: current.title.rendered}} />
+            <section className="post__entry" dangerouslySetInnerHTML={{__html: current.content.rendered}} />
+          </article>
+        )}
+        <section className="post__nav">
+          <Button>Previous Post</Button>
+          <Button>Next Post</Button>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default Post
