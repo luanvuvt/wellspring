d46b465e1df42f0abceca0c3d419d0b04370b715
Radek Stangel <radek@radekstangel.com>
2016-09-01 10:29:43 +0200

    Restructure app shell to better serve off-canvas layout

--- index.php
+++ index.php
@@ -16,24 +16,23 @@
 		<?php wp_head(); ?>
 	</head>
 	<body>
-        <!-- Top Bar -->
-        <section class="topbar">
-        	<div class="container">
-                <a class="topbar__title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-        			<?php bloginfo( 'name' ); ?>
-        	    </a>
-                <button class="topbar__toggle" href="#">
-        			<div class="topbar__toggle--text">Menu</div>
-                    <div class="topbar__toggle--icon">
-                        <span class="iconbar"></span>
-                        <span class="iconbar"></span>
-                        <span class="iconbar"></span>
-                    </div>
-        	    </button>
-            </div>
-        </section>
-        <!-- Off-Canvas Navigation -->
+        <!-- Navigation -->
         <nav class="nav">
+            <header class="nav__header">
+            	<div class="container">
+                    <a class="nav__title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            			<?php bloginfo( 'name' ); ?>
+            	    </a>
+                    <button class="nav__toggle" href="#">
+            			<div class="nav__toggle__text">Menu</div>
+                        <div class="nav__toggle__icon">
+                            <span class="iconbar"></span>
+                            <span class="iconbar"></span>
+                            <span class="iconbar"></span>
+                        </div>
+            	    </button>
+                </div>
+            </header>
             <ul class="nav__menu">
         		<?php
         	    wp_nav_menu(array(
@@ -55,13 +54,14 @@
                 Copyright &copy; 1600&ndash;<?php echo date('Y'); ?> FriendlyCorp Inc. <?php esc_html_e( 'All right reserved.', 'wellspring' ); ?>
             </div>
 		</footer>
-		<!-- Off Canvas Navigation Control -->
+
+		<!-- Off-Canvas Toggle Control -->
         <script>
-            var toggle = document.querySelector(".topbar__toggle");
-            var menu = document.querySelector(".nav");
+            var toggle = document.querySelector('.nav__toggle');
+            var content = document.getElementById('content');
             toggle.addEventListener('click', function(e) {
                 toggle.classList.toggle('close');
-                menu.classList.toggle('opened');
+                content.classList.toggle('reveal');
                 e.preventDefault();
             }, false);
         </script>
