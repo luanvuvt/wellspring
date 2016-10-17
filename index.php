49e5012f53b1fbdb5949a5d3b9feff919991756d
Radek Stangel <radek@radekstangel.com>
2016-10-17 09:04:17 +0200

    Fix PHP formatting

--- index.php
+++ index.php
@@ -10,63 +10,63 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<?php wp_head(); ?>
-	</head>
-	<body>
-        <!-- Navigation -->
-        <nav class="nav">
-        	<div class="container">
-	            <header class="nav__header">
-                    <a class="nav__title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-            			<?php bloginfo( 'name' ); ?>
-            	    </a>
-                    <button class="nav__toggle" href="#">
-                        <span>
-                			<div class="nav__toggle__text">Menu</div>
-                            <div class="nav__toggle__icon">
-                                <span class="iconbar"></span>
-                                <span class="iconbar"></span>
-                                <span class="iconbar"></span>
-                            </div>
-                        </span>
-            	    </button>
-	            </header>
-	            <ul class="nav__menu">
-	        		<?php
-	        	    wp_nav_menu(array(
-	        	        'theme_location' => 'main_nav',
-	        	        'depth'          => 1,
-	        			'container'		 => 0,
-	                    'items_wrap'     => '%3$s',
-	        		));
-	        		?>
-	            </ul>
-            </div>
-        </nav>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="nav">
+	    <div class="container">
+            <header class="nav__header">
+                <a class="nav__title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                    <?php bloginfo( 'name' ); ?>
+                </a>
+                <button class="nav__toggle">
+                    <span>
+                        <div class="nav__toggle__text">Menu</div>
+                        <div class="nav__toggle__icon">
+                            <span class="iconbar"></span>
+                            <span class="iconbar"></span>
+                            <span class="iconbar"></span>
+                        </div>
+                    </span>
+                </button>
+            </header>
+            <ul class="nav__menu">
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'main_nav',
+                    'depth'          => 1,
+                    'container'		 => 0,
+                    'items_wrap'     => '%3$s',
+                ));
+                ?>
+            </ul>
+        </div>
+    </nav>
 
-		<!-- Content -->
-		<main id="content" class="container"></main>
+    <!-- Content -->
+    <main id="content" class="container"></main>
 
-		<!-- Footer -->
-		<footer class="footer">
-            <div class="container">
-                Copyright &copy; <?php echo date('Y'); ?> Radek Stangel. <?php esc_html_e( 'All right reserved.', 'wellspring' ); ?>
-            </div>
-		</footer>
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="container">
+            Copyright &copy; <?php echo date('Y'); ?> Radek Stangel. <?php esc_html_e( 'All right reserved.', 'wellspring' ); ?>
+        </div>
+    </footer>
 
-		<!-- Off-Canvas Toggle Control -->
-        <script>
-            var toggle = document.querySelector('.nav__toggle');
-            var body = document.querySelector('body');
-            toggle.addEventListener('click', function(e) {
-                toggle.classList.toggle('close');
-                body.classList.toggle('nav-opened');
-                e.preventDefault();
-            }, false);
-        </script>
-		<?php wp_footer() ?>
-	</body>
+    <!-- Off-Canvas Toggle Control -->
+    <script>
+        var toggle = document.querySelector('.nav__toggle');
+        var body = document.querySelector('body');
+        toggle.addEventListener('click', function(e) {
+            toggle.classList.toggle('close');
+            body.classList.toggle('nav-opened');
+            e.preventDefault();
+        }, false);
+    </script>
+    <?php wp_footer() ?>
+</body>
 </html>
