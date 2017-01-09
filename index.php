d664356f70eec9f40b1c0d2920e11fb0f89941f3
Radek Stangel <radek@radekstangel.com>
2017-01-09 13:00:51 +0100

    Fix paths to the theme root in the head

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Manifest & Icons -->
-    <link rel="manifest" href="manifest.json">
-    <link rel="icon" type="image/png" href="/icons/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/icons/favicon-16x16.png" sizes="16x16">
-    <link rel="shortcut icon" href="/icons/favicon.ico">
-    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
-    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#1d3545">
-    <meta name="msapplication-config" content="browserconfig.xml">
+    <link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() ); ?>/manifest.json">
+    <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>//icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>//icons/favicon-16x16.png" sizes="16x16">
+    <link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>//icons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() ); ?>//icons/apple-touch-icon.png">
+    <link rel="mask-icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>//icons/safari-pinned-tab.svg" color="#1d3545">
+    <meta name="msapplication-config" content="<?php echo esc_url( get_template_directory_uri() ); ?>/browserconfig.xml">
     <meta name="theme-color" content="#1d3545">
 	<?php wp_head(); ?>
 </head>
