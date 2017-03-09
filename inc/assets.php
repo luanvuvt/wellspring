a7c331f8e2f89875d4d3fa4fbc4a9ff5ee690123
Radek Stangel <radek@radekstangel.com>
2017-03-09 11:15:51 +0100

    Remove separate React libs since we bundle them now

--- inc/assets.php
+++ inc/assets.php
@@ -21,9 +21,22 @@ function wellspring_remove_jquery_migrate( &$scripts ) {
 add_filter( 'wp_default_scripts', 'wellspring_remove_jquery_migrate' );
 
 /**
+ * App scripts
+ *
+ * Enqueues app scripts to the footer
+ *
+ * @since 1.0.0
+ */
+function wellspring_scripts() {
+	// wellspring app bundle
+	wp_enqueue_script( 'wellspring', get_template_directory_uri() . '/dist/app.js', array(), null, true );
+}
+add_action( 'wp_enqueue_scripts', 'wellspring_scripts' );
+
+/**
  * App shell assets
  *
- * Enqueues all application shell assets
+ * WILL BE DEPRACATED WITH STYLE COMPONENTS!!!
  *
  * @since 1.0.0
  */
@@ -32,22 +45,3 @@ function wellspring_appshell_assets() {
 	wp_enqueue_style( 'wellspring-appshell', get_template_directory_uri() . '/dist/appshell.css', array(), THEME_VERSION );
 }
 add_action( 'wp_enqueue_scripts', 'wellspring_appshell_assets' );
-
-/**
- * Content assets
- *
- * Enqueues all content assets to the footer
- *
- * @since 1.0.0
- */
-function wellspring_content_assets() {
-	// react
-	wp_enqueue_script( 'react', get_template_directory_uri() . '/dist/vendor/react.min.js', array(), '15.3.0', true );
-
-	// react dom
-	wp_enqueue_script( 'react-dom', get_template_directory_uri() . '/dist/vendor/react-dom.min.js', array(), '15.3.0', true );
-
-	// wellspring content bundle
-	wp_enqueue_script( 'wellspring-content', get_template_directory_uri() . '/dist/content.js', array('react', 'react-dom'), THEME_VERSION, true );
-}
-add_action( 'wp_footer', 'wellspring_content_assets' );
