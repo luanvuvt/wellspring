35ab4c5f8e7f2c5f0d1b753ef3387420e6c390d1
Radek Stangel <radek@radekstangel.com>
2017-03-16 10:40:43 +0100

    :art: Create style settings

---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+// theme settings
+
+export const color = {
+  primary: '#077dc9',
+  contrast: '#ea4429',
+  white: '#f8fbfd',        // mix(#fff, primary, 97%)
+  black: '#20292f',        // mix(#222, primary, 92%)
+  blackPrimary: '#1d3240', // mix(#222, primary, 82%)
+  blackLight: '#3f494f',   // mix(#444, primary, 92%)
+  gray: '#bcc6cc',         // mix(#ccc, primary, 92%)
+};
+
+export const font = {
+  family: {
+    base: '"Helvetica Neue", Helvetica, Roboto, Arial, sans-serif',
+    light: '"HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif',
+    mono: '"Liberation Mono", Courier, monospace',
+  },
+  size: {
+    base: '1rem',
+    small: '1.125rem',
+    medium: '1.25rem',
+    large: '1.375rem',
+  },
+  weight: {
+    light: '300',
+    normal: '400',
+    bold: '700',
+  },
+  lineHeight: 1.5,
+};
+
+export const radius = '3px';
