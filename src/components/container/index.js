6cfec56f30c8f64a092ccb14bfc193b9c059a0c5
Radek Stangel <radek@radekstangel.com>
2017-03-16 11:17:35 +0100

    Create container component

---
+++ src/components/container/index.js
@@ -0,0 +1,16 @@
+// dependencies
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 0 1em;
+  margin: 0 auto;
+  max-width: 968px;
+  min-width: 320px;
+  width: 100%;
+
+  @media (min-width: 545px) {
+    padding: 0 2em;
+  }
+`;
+
+export default Container;
