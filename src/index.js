06807faa135f1a6014dcd8aeed7da8c24be4417e
Radek Stangel <radek@radekstangel.com>
2017-03-16 11:57:19 +0100

    Add Content component to the router layout

--- src/index.js
+++ src/index.js
@@ -9,14 +9,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 // containers
+import Blog from './containers/blog';
 import Home from './containers/home';
 import Page from './containers/page';
-import Blog from './containers/blog';
 import Post from './containers/post';
 
 // components
-import Navigation from './components/navigation';
+import Content from './components/content';
 import Footer from './components/footer';
+import Navigation from './components/navigation';
 
 // styles
 import './styles/base';
@@ -24,16 +25,17 @@ import './styles/base';
 ReactDOM.render(
   <BrowserRouter>
     <div>
-      <hr />
       <Navigation />
-      <Route exact path="/" component={Home} />
-      <Route path="/blog" component={Blog} />
-      <Route path="/blog/:slug" component={Post} />
-      <Route path="/:slug" component={Page} />
+      <Content>
+        <Route exact path="/" component={Home} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/blog/:slug" component={Post} />
+        <Route path="/:slug" component={Page} />
+      </Content>
       <Footer />
     </div>
   </BrowserRouter>,
-  document.getElementById('content')
+  document.getElementById('app')
 );
 
 // init offline support
