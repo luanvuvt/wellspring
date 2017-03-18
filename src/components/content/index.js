89f83724169aaeab4a88797bcdb73ccf7f846575
Radek Stangel <radek@radekstangel.com>
2017-03-18 14:09:54 +0100

    Add content/off-canvas transition styles

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -12,7 +12,21 @@ const Content = styled(Container)`
   flex: 1 0 auto; /* sticky footer */
   padding-bottom: 1.5em;
   padding-top: 1.5em;
+  transition: 0.35s; /* off-canvas transition */
   z-index: 1; /* above off-canvas */
+
+  /* content overlay when nav is opened */
+  &:after {
+    background: ${color.black};
+    content: "";
+    height: 0;
+    left: 0;
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    width: 0;
+    transition: opacity 0.35s, width 0.1s 0.35s, height 0.1s 0.35s;
+  }
 `;
 
 export default Content;
