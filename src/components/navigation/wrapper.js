78c2d5cd38b0b3a80a012a71e53e4922e84d7cf4
Radek Stangel <radek@radekstangel.com>
2017-03-18 15:59:02 +0100

    Add navigation wrapper

---
+++ src/components/navigation/wrapper.js
@@ -0,0 +1,11 @@
+// dependencies
+import styled from 'styled-components';
+
+// styles
+import { color } from '../../styles/theme';
+
+const Wrapper = styled.nav`
+  background: ${color.blackPrimary};
+`;
+
+export default Wrapper;
