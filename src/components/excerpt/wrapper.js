94caf19622b34200e1b74a96d5fc0ee5c12db078
Radek Stangel <radek@radekstangel.com>
2017-03-18 14:10:25 +0100

    Use theme settings in the excerpt wrapper

--- src/components/excerpt/wrapper.js
+++ src/components/excerpt/wrapper.js
@@ -1,8 +1,11 @@
 // dependecies
 import styled from 'styled-components';
 
+// styles
+import { color } from '../../styles/theme';
+
 const Wrapper = styled.article`
-  border: 1px solid #ccd7dd;
+  border: 1px solid ${color.gray};
   border-radius: 3px;
   padding: 1em;
 `;
