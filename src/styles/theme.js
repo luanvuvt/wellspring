73dc0c1dce11260dd47bcf123a10c7b065449243
Radek Stangel <radek@radekstangel.com>
2017-03-18 12:55:38 +0100

    Add style navigation settings

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,15 +4,16 @@
 export const color = {
   primary: '#077dc9',
   contrast: '#ea4429',
-  primaryDark: '#066eb0',  // darken(primary, 5%)
-  contrastDark: '#e33316', // darken(contrast, 5%)
-  white: '#f8fbfd',        // mix(#fff, primary, 97%)
-  black: '#20292f',        // mix(#222, primary, 92%)
-  blackPrimary: '#1d3240', // mix(#222, primary, 82%)
-  blackLight: '#3f494f',   // mix(#444, primary, 92%)
-  gray: '#bcc6cc',         // mix(#ccc, primary, 92%)
-  grayDark: '#61707a',     // lighten(blackLight, 15%)
-  grayLight: '#e1e6e9'     // mix(#eaeaea, primary, 96%)
+  primaryDark: '#066eb0',  // darken(primary, 5%) ... primary mouse over
+  contrastDark: '#e33316', // darken(contrast, 5%) ... contrast mouse over
+  white: '#f8fbfd',        // mix(#fff, primary, 97%) ... content bg
+  black: '#20292f',        // mix(#222, primary, 92%) ... headings
+  blackPrimary: '#1d3240', // mix(#222, primary, 82%) ... app shell
+  blackLight: '#3f494f',   // mix(#444, primary, 92%) ... text
+  gray: '#bcc6cc',         // mix(#ccc, primary, 92%) ... excerpt border
+  grayPrimary: '#244b65',  // mix(#333, primary, 67%) ... off-canvas bg
+  grayDark: '#61707a',     // lighten(blackLight, 15%) ... quote text
+  grayLight: '#e1e6e9'     // mix(#eaeaea, primary, 96%) ... pre, code bg
 };
 
 // font and text parameters
@@ -35,6 +36,15 @@ export const media = {
   small: '@media (min-width: 34.0625em)',  // 545px
   medium: '@media (min-width: 42.8125em)', // 685px
   large: '@media (min-width: 52.3125em)',  // 837px
+};
+
+// navigation
+export const nav = {
+  offcanvasWidth: '14rem',  // 224px
+  offcanvasBorder: '1px solid rgba(#fff, 0.15)',
+  topbarHeight: '3.125rem', // 50px
+  topbarBorder: '1px solid rgba(#fff, 0.2)',
+  transitionTime: '0.35s',
 };
 
 // global radius
