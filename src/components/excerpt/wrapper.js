b28ecec1e84f5b309561dbc8baca46e9afa6db3c
Radek Stangel <radek@radekstangel.com>
2017-03-31 20:06:32 +0200

    Add min-height to excerpts

--- src/components/excerpt/wrapper.js
+++ src/components/excerpt/wrapper.js
@@ -2,12 +2,16 @@
 import styled from 'styled-components';
 
 // styles
-import { color } from '../../styles/theme';
+import { color, media } from '../../styles/theme';
 
 const Wrapper = styled.article`
   border: 1px solid ${color.gray};
   border-radius: 3px;
   padding: 1em;
+
+  ${media.medium} {
+    min-height: 24em;
+  }
 `;
 
 export default Wrapper;
