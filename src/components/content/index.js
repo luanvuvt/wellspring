bf4049032de1d66907fd2b20b696aac51eceb207
Radek Stangel <radek@radekstangel.com>
2017-04-03 22:11:23 +0200

    Add transition styles to the Content component

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -5,12 +5,13 @@ import styled from 'styled-components';
 import Box from '../box';
 
 // styles
-import { color, media } from '../../styles/theme';
+import { color, media, nav } from '../../styles/theme';
 
 const Content = styled(Box)`
   background: ${color.white};
   flex: 1 0 auto; /* sticky footer */
   padding: 1.5em 1em;
+  transform: ${props => props.navIsOpen ? `translate3d(-${nav.offcanvasWidth}, 0, 0)` : 'none'};
   transition: 0.35s; /* off-canvas transition */
   z-index: 1; /* above off-canvas */
 
@@ -22,13 +23,13 @@ const Content = styled(Box)`
   &:after {
     background: ${color.black};
     content: "";
-    height: 0;
+    height: ${props => props.navIsOpen ? '100%' : '0'};
     left: 0;
-    opacity: 0;
+    opacity: ${props => props.navIsOpen ? '0.4' : '0'};
     position: absolute;
     top: 0;
-    width: 0;
-    transition: opacity 0.35s, width 0.1s 0.35s, height 0.1s 0.35s;
+    transition: ${props => props.navIsOpen ? 'opacity 0.35s' : 'opacity 0.35s, width 0.1s 0.35s, height 0.1s 0.35s'};
+    width: ${props => props.navIsOpen ? '100%' : '0'};
   }
 `;
 
