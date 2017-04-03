d90e129e318ea66e72f03c16309d72be27f284ef
Radek Stangel <radek@radekstangel.com>
2017-04-03 22:11:38 +0200

    Animate toggle icon

--- src/components/navigation/toggle-icon-bar.js
+++ src/components/navigation/toggle-icon-bar.js
@@ -12,6 +12,18 @@ const ToggleIconBar = styled.div`
   margin-bottom: ${4 / 16}rem;
   transition: transform 0.3s;
   width: ${22 / 16}rem;
+
+  &:nth-child(1) {
+    transform: ${props => props.navIsOpen ? 'translateY(6px) rotate(-45deg)' : 'none'};
+  }
+
+  &:nth-child(2) {
+    transform: ${props => props.navIsOpen ? 'scale(0)' : 'none'};
+  }
+
+  &:nth-child(3) {
+    transform: ${props => props.navIsOpen ? 'translateY(-6px) rotate(45deg)' : 'none'};
+  }
 `;
 
 export default ToggleIconBar;
